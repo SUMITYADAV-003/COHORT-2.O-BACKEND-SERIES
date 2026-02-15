71e10573d43baf90dbccbf2f61b00c98af1fe166
Sumit yadav <sumitkumaryadav9608700@gmail.com>
2026-02-15 21:33:09 +0530

    15 feb

--- DAY-6/Backend/src/app.js
+++ DAY-6/Backend/src/app.js
@@ -1,13 +1,15 @@
 const express = require("express");
 const noteModel = require("./modles/Notes.modles.js");
+const cors = require("cors");
 
 
 const app = express();
 
 
 app.use(express.json());
+app.use(cors());
 // create new note
-app.post("/notes",  async (req,res) => {
+app.post("/api/notes",  async (req,res) => {
   const {title,description,age} = req.body;
   const note = await noteModel.create({
     title,description,age
@@ -20,12 +22,47 @@ app.post("/notes",  async (req,res) => {
 })
 
 // factch all notes
-app.get("/notes", async (req,res) => {
+app.get("/api/notes", async (req,res) => {
   let note = await noteModel.find();
   res.status(201).json({
     message: "Get all Notes",
     note,
   })
 })
+// delete notes using id
+app.delete("/api/notes/:id", async (req,res) => {
+  const id = req.params.id;
+  await noteModel.findByIdAndDelete(id); 
+  res.status(200).json({
+    message: "Notes Deleted successfully",
+  })
+})
+
+/*
+ - PATH /api/notes/:id
+ - Update the descriptions of the note
+ - req.body  = {descriptions}
+*/
+
+app.patch("/api/notes/:id", async (req,res) => {
+  const id = req.params.id;
+  const {description}  =req.body;
+  await noteModel.findByIdAndUpdate(id, {description});
+  res.status(200).json({
+    message: "Update Successfully ",
+  })
+  
+})
+
+
+
+
+
+
+
+
+
+
+
 
 module.exports = app;
--- DAY-6/Frontend/src/App.jsx
+++ DAY-6/Frontend/src/App.jsx
@@ -1,35 +1,31 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import "./app.scss";
+import { useEffect, useState } from "react";
+import axios from "axios";
 
-function App() {
-  const [count, setCount] = useState(0)
+const App = () => {
+  const [notes, setNotes] = useState([]);
+
+  useEffect(() => {
+    axios.get("http://localhost:3001/api/notes").then((res) => {
+      setNotes(res.data.note).catch((err) => {
+        console.error("Error:", err);
+      });
+    });
+  }, []);
 
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
-}
+    <div className="notes">
+      {notes.map((note) => {
+        return (
+          <div className="note">
+            <p>{note.title}</p>
+            <p>{note.description}</p>
+            <h3>{note.age}</h3>
+          </div>
+        );
+      })}
+    </div>
+  );
+};
 
-export default App
+export default App;
