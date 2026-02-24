d3941b9da08b57d84caecacf189dcfcf6316bf1c
Sumit yadav <sumitkumaryadav9608700@gmail.com>
2026-02-24 23:56:49 +0530

    24 feb

--- DAY-10/Frontend/src/App.jsx
+++ DAY-10/Frontend/src/App.jsx
@@ -1,10 +1,18 @@
 import React from 'react'
+import CreteUser from './Users/CreteUser'
+import { Routes, Route } from 'react-router-dom'
 
 const App = () => {
   return (
     <>
     <main>
-      
+      <Routes>
+        
+        <Route path="/" element={<CreteUser />} />
+         
+        
+      </Routes>
+
     </main>
     </>
   )
