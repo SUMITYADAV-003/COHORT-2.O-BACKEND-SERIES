abdd86578fa6304f364c7630a2fd13294d35032f
Sumit yadav <sumitkumaryadav9608700@gmail.com>
2026-03-19 21:11:55 +0530

    complete (userAuth) and AuthContext;

--- PROJECT-1/Frontend/src/features/auth/Hook/useAuth.js
+++ PROJECT-1/Frontend/src/features/auth/Hook/useAuth.js
@@ -1,2 +1,11 @@
-import {useContext}  from "react";
-import
+import {useContext} from "react";
+import { AuthContext } from "../AuthContext";
+import {login, register, getme} from "../services/auth.api";
+
+
+export const userAuth () => {
+  const context = useContext(AuthContext);
+
+  const {user, setUser, loading, setLoading} = context;
+
+}
