f27731cf3dde73181067f8d93cf9239e1f94ad4f
Sumit yadav <sumitkumaryadav9608700@gmail.com>
2026-03-25 23:18:01 +0530

    25 mar

--- PROJECT-1/Frontend/src/features/posts/pages/Feed.jsx
+++ PROJECT-1/Frontend/src/features/posts/pages/Feed.jsx
@@ -1,8 +1,25 @@
 import React from 'react'
+import "../style/feed.scss"
 
 const Feed = () => {
   return (
-    <div>Feed</div>
+    <main className='feed-controller'>
+      <div className="feed">
+        <div className="users">
+        <div className="user">
+          <img src="img.jpg" alt="" />
+          <h1>username</h1>
+        </div>
+        </div>
+
+         <div className="posts">
+          <div className="post">
+            <img src="img1.jpg" alt="" />
+          </div>
+        </div>
+       
+      </div>
+    </main>
   )
 }
 
