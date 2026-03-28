c897a55eee64e27fccf6e19ee4bb9c9e30172df0
Sumit yadav <sumitkumaryadav9608700@gmail.com>
2026-03-28 13:33:43 +0530

    ui features like a post create only i

---
+++ PROJECT-1/Frontend/src/features/posts/pages/CreatePost.jsx
@@ -0,0 +1,15 @@
+import React, {useState, useRef} from 'react';
+import "../style/createpost.scss";
+import {usePost} from "../hooks/usePost";
+import {useNavigate} from "react-dom";
+
+const CreatePost = () => {
+  return (
+    <main className='create-post-page'>
+      <div></div>
+
+    </main>
+  )
+}
+
+export default CreatePost
