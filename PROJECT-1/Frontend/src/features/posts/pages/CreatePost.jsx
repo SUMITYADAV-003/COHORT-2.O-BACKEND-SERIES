7160f5aa03e47e93b65c95c3f164b0739d540007
Sumit yadav <sumitkumaryadav9608700@gmail.com>
2026-03-28 13:45:39 +0530

    ui features like a post create only i

--- PROJECT-1/Frontend/src/features/posts/pages/CreatePost.jsx
+++ PROJECT-1/Frontend/src/features/posts/pages/CreatePost.jsx
@@ -1,15 +1,38 @@
-import React, {useState, useRef} from 'react';
+import React, { useState, useRef } from "react";
 import "../style/createpost.scss";
-import {usePost} from "../hooks/usePost";
-import {useNavigate} from "react-dom";
+import { usePost } from "../hooks/usePost";
+import { useNavigate } from "react-dom";
 
 const CreatePost = () => {
+  const [caption, setCaption] = useState("");
+  const   = useState("");
+
+
+
+
   return (
-    <main className='create-post-page'>
-      <div></div>
-
+    <main className="create-post-page">
+      <div className="form-container">
+        <h1>Crete post </h1>
+        <form onSubmit={handleSubmit}>
+          <label className="post-image-label" htmlFor="postImage">
+            Select image
+          </label>
+          <input
+            ref={postImageInputFieldRef}
+            type="file"
+            hidden
+            name="postImage"
+          />
+          <input 
+          type="text" 
+          value={caption}
+          onChange={(e) => {sel}}
+          placeholder="Enter Caption" />
+        </form>
+      </div>
     </main>
-  )
-}
+  );
+};
 
-export default CreatePost
+export default CreatePost;
